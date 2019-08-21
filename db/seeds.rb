c7e3559e327db8864d268fa0dde3ba8f32774784
Masahiro1 <masahiro.togashi@gmail.com>
2019-08-21 14:27:33 +0900

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# reset
+User.all.destroy_all
+Workspace.all.destroy_all
+Channel.all.destroy_all
+
+# channelを10こもつ　workspace1つ　を1つずつもつ user を10作成
+10.times do |i|
+  user = User.create!(
+    email: "user#{i}@example.com",
+    password: "password",
+    name: "user#{i}",
+  )
+
+  workspace = user.workspaces.build(
+    name: "workspace#{i}"
+  )
+  workspace.save!
+
+  10.times do |j|
+    channel = workspace.channels.build(
+      name: "channel#{i}_#{j}"
+    )
+    channel.save!
+  end
+end
+
+puts "finish create users, workspaces, channels"
+
+# 全部の user を 全部の workspace、channel に登録
+users = User.order(created_at: :asc)
+workspaces = Workspace.order(created_at: :asc)
+channels = Channel.order(created_at: :asc)
+
+users.each do |u|
+  workspaces.each do |w|
+    UserWorkspace.create!(user_id: u.id, workspace_id: w.id)
+  end
+
+  channels.each do |c|
+    UserChannel.create!(user_id: u.id, channel_id: c.id)
+  end
+end
+
+puts "finish connect users and workspaces, channels"
