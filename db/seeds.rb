6e86d102ad25a125b9daf74e29ceb1c496e719ad
Masahiro1 <masahiro.togashi@gmail.com>
2019-08-21 16:34:35 +0900

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,7 @@
 User.all.destroy_all
 Workspace.all.destroy_all
 Channel.all.destroy_all
+Message.all.destroy_all
 
 # channelを10こもつ　workspace1つ　を1つずつもつ user を10作成
 10.times do |i|
@@ -42,3 +43,17 @@ users.each do |u|
 end
 
 puts "finish connect users and workspaces, channels"
+
+channels = Channel.order(created_at: :asc)
+users    = User.order(created_at: :asc)
+channels.each do |c|
+  users.all.each do |u|
+    Message.create!(
+      user_id: u.id,
+      channel_id: c.id,
+      content: "My name is #{u.name}. I'm speaking in channel #{c.name}."
+    )
+  end
+end
+
+puts "finish create messages"
