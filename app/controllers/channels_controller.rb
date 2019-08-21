6abb8ff912e5e41b665566db5c544c4d259c4012
Masahiro1 <masahiro.togashi@gmail.com>
2019-08-21 14:27:59 +0900

    add channels home show

---
+++ app/controllers/channels_controller.rb
@@ -0,0 +1,20 @@
+class ChannelsController < ApplicationController
+  before_action :authenticate_user!
+
+  def home; end
+
+  def show
+    @user = current_user
+    @workspaces = @user.workspaces.includes(:channels).order(created_at: :asc)
+
+    id = params[:channel_id]
+    if id.present?
+      @channel = Channel.find(id)
+    else
+      @channel = @workspaces.first.channels.first
+    end
+
+    @workspace = @channel.workspace
+    @channels  = @workspace.channels.order(created_at: :asc)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :channels do
+    get 'home'
+    get 'show'
+  end
+
+  root 'channels#home'
 end
