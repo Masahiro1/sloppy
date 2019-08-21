1a4ecbc8a9fd4ab16e0ec35883ba3df1132323db
Masahiro1 <masahiro.togashi@gmail.com>
2019-08-21 16:34:25 +0900

    add message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_21_004054) do
+ActiveRecord::Schema.define(version: 2019_08_21_053028) do
 
   create_table "channels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.bigint "workspace_id"
@@ -18,6 +18,17 @@ ActiveRecord::Schema.define(version: 2019_08_21_004054) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["workspace_id"], name: "index_channels_on_workspace_id"
+  end
+
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+    t.string "content"
+    t.string "image"
+    t.bigint "user_id"
+    t.bigint "channel_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["channel_id"], name: "index_messages_on_channel_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "user_channels", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
@@ -57,4 +68,6 @@ ActiveRecord::Schema.define(version: 2019_08_21_004054) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "messages", "channels"
+  add_foreign_key "messages", "users"
 end
